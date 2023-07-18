3a59f0743361bef972a441c1a463b5fcba267e46
elisavetterz <eterzani.atm@gmail.com>
2023-07-18 16:39:44 +0300

    payments with Stripe-done

--- server/middlewares/requireLogin.js
+++ server/middlewares/requireLogin.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
-  if (!req.res) {
+  if (!req.user) {
     return res.status(401).send({ error: "You must log in!" });
   }
-
+  //next is a function that we call when our middleware is finished running
   next();
 };
