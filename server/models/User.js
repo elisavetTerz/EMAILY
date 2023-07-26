25bec66ae1763fda0aee4df4245eb59c747f0b4f
elisavetterz <eterzani.atm@gmail.com>
2023-07-26 09:20:28 +0300

    Added billing and client side app

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,5 @@
 //create mongooose model class which is the User collection
 const mongoose = require("mongoose");
-// const mongoose = require("mongoose");
 const { Schema } = mongoose;
 //const Schema = mongoose.Schema; is the same to line 3
 const userSchema = new Schema({
